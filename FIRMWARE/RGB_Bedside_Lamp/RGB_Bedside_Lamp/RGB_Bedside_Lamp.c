53b577dc3cfa0762ceb1467567241f80b25472ea
denis-m-tyurin <denis.m.turin@gmail.com>
2014-09-28 22:47:29 +0400

    Software PWM implementation started
    Signed-off-by: denis-m-tyurin <denis.m.turin@gmail.com>

--- FIRMWARE/RGB_Bedside_Lamp/RGB_Bedside_Lamp/RGB_Bedside_Lamp.c
+++ FIRMWARE/RGB_Bedside_Lamp/RGB_Bedside_Lamp/RGB_Bedside_Lamp.c
@@ -11,14 +11,134 @@
 #include <avr/io.h>
 #include <stdlib.h>
 #include <util/delay.h>
-#include "leds.h"
+#include <avr/interrupt.h>
+
+#define PWM_TOP 1000
+#define HUE_TOP 1536
+#define PWM_NIGHT_MODE_REPEAT_CYCLES 100
+#define PWM_TRANSITION_MODE_REPEAT_CYCLES 1
+
+#define RED_LED_ON PORTD |= (1 << PD6)
+#define RED_LED_OFF PORTD &= ~(1 << PD6)
+
+#define BLUE_LED_ON PORTD |= (1 << PD5)
+#define BLUE_LED_OFF PORTD &= ~(1 << PD5)
+
+#define GREEN_LED_ON PORTB |= (1 << PB3)
+#define GREEN_LED_OFF PORTB &= ~(1 << PB3)
 
 struct RGB
 {
-	uint8_t red;
-	uint8_t green;
-	uint8_t blue;
+	uint16_t red;
+	uint16_t green;
+	uint16_t blue;
 };
+
+static uint8_t mode_switch = 0;
+
+struct RGB HSV2RGB_Adv1 (uint16_t hue, unsigned char sat, unsigned char val);
+
+int main(void)
+{
+	struct RGB color;
+	uint16_t hue;
+	uint16_t pwm_counter;
+	
+	uint16_t pwm_cycles;
+
+	/* BLUE: PD5
+	   RED: PD6
+	   GREEN: PB3
+   
+	   Note that these PINs are connected to output units of Timer0/Timer2
+       therefore we could use hardware PWM mode. But since it has low resolution
+       we would use software generated PWM. */
+	DDRD |= (1<<PD5) | (1<<PD6);
+	DDRB |= (1<<PB3);
+		
+	RED_LED_OFF;
+	GREEN_LED_OFF;
+	BLUE_LED_OFF;
+
+	// Configure interrupts for buttons	
+	// PD3 (INT1) - Power toggle button
+	// PD2 (INT0) - Mode switch 
+	PORTD |= (1 << PD3) | (1 << PD2); // Enable internal pull-ups for these PINs
+	EICRA |= (1 << ISC01); // INT1 - low level generates INT, INT0 - falling edge generates INT
+	EIMSK |= (1 << INT1) | (1 << INT0);
+
+	
+	// Enable interrupts
+	sei();
+			
+    while(1)
+    {
+		mode_switch = 0;
+		
+		// 1st mode - night lamp		
+		{
+			if (1 == mode_switch) break;
+			
+			for (hue = 0; hue < HUE_TOP; hue++)
+			{
+				if (1 == mode_switch) break;
+				
+				color = HSV2RGB_Adv1(hue, 255, 255);
+				for (pwm_cycles = 0; pwm_cycles < PWM_NIGHT_MODE_REPEAT_CYCLES; pwm_cycles++)
+				{
+					RED_LED_ON;
+					GREEN_LED_ON;
+					BLUE_LED_ON;
+		
+					for (pwm_counter = 0; pwm_counter<PWM_TOP; pwm_counter++)
+					{
+						if (pwm_counter == color.red)	RED_LED_OFF;
+						if (pwm_counter == color.green)	GREEN_LED_OFF;
+						if (pwm_counter == color.blue)	BLUE_LED_OFF;
+					}
+				}
+			}
+		} while (mode_switch != 1);
+		
+		mode_switch = 0;
+	#if 0	
+		// This should never happen, but make the level compliant to PWM TOP
+		if (color.red > PWM_TOP) color.red = PWM_TOP;
+		if (color.green > PWM_TOP) color.green = PWM_TOP;
+		if (color.blue > PWM_TOP) color.blue = PWM_TOP;
+		
+		// Increase LEDs level
+		{
+			for (pwm_cycles = 0; pwm_cycles < PWM_TRANSITION_MODE_REPEAT_CYCLES; pwm_cycles++)
+			{
+				RED_LED_ON;
+				GREEN_LED_ON;
+				BLUE_LED_ON;
+				
+				for (pwm_counter = 0; pwm_counter<PWM_TOP; pwm_counter++)
+				{
+					if (pwm_counter == color.red)	RED_LED_OFF;
+					if (pwm_counter == color.green)	GREEN_LED_OFF;
+					if (pwm_counter == color.blue)	BLUE_LED_OFF;
+				}
+			}
+			
+			if (color.red < PWM_TOP) color.red++;
+			if (color.green < PWM_TOP) color.green++;
+			if (color.blue < PWM_TOP) color.blue++;
+						
+		} while (color.red < PWM_TOP || color.green < PWM_TOP || color.blue < PWM_TOP);		
+#endif		
+		// 2nd mode - regular white lamp
+		RED_LED_ON;
+		GREEN_LED_ON;
+		BLUE_LED_ON;
+		{
+			_delay_ms(5);
+		} while (1 != mode_switch);
+	}
+}
+
 
 struct RGB HSV2RGB_Adv1 (uint16_t hue, unsigned char sat, unsigned char val)
 {
@@ -73,43 +193,12 @@ struct RGB HSV2RGB_Adv1 (uint16_t hue, unsigned char sat, unsigned char val)
 	return out;
 } // HSV 2 RGB Binary
 
-
-int main(void)
+ISR(INT0_vect)
 {
-	struct RGB color;
-	uint16_t hue;
+	mode_switch = 1;
+}
+
+ISR(INT1_vect)
+{
 	
-	leds_init();
-			
-    while(1)
-    {
-		for (hue = 0; hue < 1536; hue++)
-		{
-			color = HSV2RGB_Adv1(hue, 255, LEDS_MAX_LEVEL);
-			leds_set_rgb(color.red, color.green, color.blue);
-			_delay_ms(15);
-		}
-		
-		for (hue = 0; hue < 1536; hue++)
-		{
-			color = HSV2RGB_Adv1(hue, 255, LEDS_NIGHT_LEVEL);
-			leds_set_rgb(color.red, color.green, color.blue);
-			_delay_ms(15);
-		}
-	
-		for (hue = 0; hue < 1536; hue++)
-		{
-			color = HSV2RGB_Adv1(hue, 255, LEDS_NIGHT_LEVEL_5);
-			leds_set_rgb(color.red, color.green, color.blue);
-			_delay_ms(15);
-		}
-		
-		for (hue = 0; hue < 1536; hue++)
-		{
-			color = HSV2RGB_Adv1(hue, 255, LEDS_NIGHT_LEVEL_1);
-			leds_set_rgb(color.red, color.green, color.blue);
-			_delay_ms(15);
-		}
-		
-    }
 }
