e260a7fdd2add32278f243cc6e0fdcc98d4798fe
denis-m-tyurin <denis.m.turin@gmail.com>
2014-09-29 22:56:48 +0400

    Software PWM - continued. Reduced stack usage.
    Signed-off-by: denis-m-tyurin <denis.m.turin@gmail.com>

--- FIRMWARE/RGB_Bedside_Lamp/RGB_Bedside_Lamp/RGB_Bedside_Lamp.c
+++ FIRMWARE/RGB_Bedside_Lamp/RGB_Bedside_Lamp/RGB_Bedside_Lamp.c
@@ -14,8 +14,8 @@
 #include <avr/interrupt.h>
 
 #define PWM_TOP 1000
-#define HUE_TOP 1536
-#define PWM_NIGHT_MODE_REPEAT_CYCLES 100
+#define HUE_TOP 256*6
+#define PWM_NIGHT_MODE_REPEAT_CYCLES 10
 #define PWM_TRANSITION_MODE_REPEAT_CYCLES 1
 
 #define RED_LED_ON PORTD |= (1 << PD6)
@@ -34,18 +34,18 @@ struct RGB
 	uint16_t blue;
 };
 
-static uint8_t mode_switch = 0;
+static volatile uint8_t mode_switch = 0;
 
-struct RGB HSV2RGB_Adv1 (uint16_t hue, unsigned char sat, unsigned char val);
+// Make the following vars global to reduce stack usage in nested loops
+static struct RGB color = {0, 0, 0};
+uint16_t hue;
+uint16_t pwm_cycles;
+uint16_t pwm_counter;
+
+void HSV2RGB_Adv1 (uint16_t hue, uint16_t sat, uint16_t val);
 
 int main(void)
 {
-	struct RGB color;
-	uint16_t hue;
-	uint16_t pwm_counter;
-	
-	uint16_t pwm_cycles;
-
 	/* BLUE: PD5
 	   RED: PD6
 	   GREEN: PB3
@@ -66,16 +66,14 @@ int main(void)
 	PORTD |= (1 << PD3) | (1 << PD2); // Enable internal pull-ups for these PINs
 	EICRA |= (1 << ISC01); // INT1 - low level generates INT, INT0 - falling edge generates INT
 	EIMSK |= (1 << INT1) | (1 << INT0);
-
 	
 	// Enable interrupts
 	sei();
 			
     while(1)
     {
-		mode_switch = 0;
-		
 		// 1st mode - night lamp		
+		for (;mode_switch != 1;)
 		{
 			if (1 == mode_switch) break;
 			
@@ -83,7 +81,8 @@ int main(void)
 			{
 				if (1 == mode_switch) break;
 				
-				color = HSV2RGB_Adv1(hue, 255, 255);
+				// This function will change the color global variable to reduce stack usage
+				HSV2RGB_Adv1(hue, 255, 255);
 				for (pwm_cycles = 0; pwm_cycles < PWM_NIGHT_MODE_REPEAT_CYCLES; pwm_cycles++)
 				{
 					RED_LED_ON;
@@ -98,16 +97,15 @@ int main(void)
 					}
 				}
 			}
-		} while (mode_switch != 1);
+		}
 		
-		mode_switch = 0;
-	#if 0	
 		// This should never happen, but make the level compliant to PWM TOP
 		if (color.red > PWM_TOP) color.red = PWM_TOP;
 		if (color.green > PWM_TOP) color.green = PWM_TOP;
 		if (color.blue > PWM_TOP) color.blue = PWM_TOP;
 		
 		// Increase LEDs level
+		for (;color.blue < PWM_TOP || color.red < PWM_TOP || color.green < PWM_TOP;)
 		{
 			for (pwm_cycles = 0; pwm_cycles < PWM_TRANSITION_MODE_REPEAT_CYCLES; pwm_cycles++)
 			{
@@ -126,25 +124,31 @@ int main(void)
 			if (color.red < PWM_TOP) color.red++;
 			if (color.green < PWM_TOP) color.green++;
 			if (color.blue < PWM_TOP) color.blue++;
-						
-		} while (color.red < PWM_TOP || color.green < PWM_TOP || color.blue < PWM_TOP);		
-#endif		
+		}
+
+		mode_switch = 0;
+		
 		// 2nd mode - regular white lamp
 		RED_LED_ON;
 		GREEN_LED_ON;
 		BLUE_LED_ON;
+		
+		for (;1 != mode_switch;)
 		{
 			_delay_ms(5);
-		} while (1 != mode_switch);
+		}
+		
+		// debouncing
+		_delay_ms(1000);
+		mode_switch = 0;
 	}
 }
 
 
-struct RGB HSV2RGB_Adv1 (uint16_t hue, unsigned char sat, unsigned char val)
+void HSV2RGB_Adv1 (uint16_t hue, uint16_t sat, uint16_t val)
 {
-	struct RGB out;
 	if (!sat) { // Acromatic color (gray). Hue doesn't mind.
-		out.red=out.green=out.blue=val;
+		color.red=color.green=color.blue=val;
 		} else  {
 
 		
@@ -153,44 +157,42 @@ struct RGB HSV2RGB_Adv1 (uint16_t hue, unsigned char sat, unsigned char val)
 		uint8_t iiii= hue/256;
 		switch(iiii) {
 			case 0:
-			out.red = val;
-			out.green = (((val-base)*hue)/256)+base;
-			out.blue = base;
+			color.red = val;
+			color.green = (((val-base)*hue)/256)+base;
+			color.blue = base;
 			break;
 
 			case 1:
-			out.red = (((val-base)*(256-(hue%256)))/256)+base;
-			out.green = val;
-			out.blue = base;
+			color.red = (((val-base)*(256-(hue%256)))/256)+base;
+			color.green = val;
+			color.blue = base;
 			break;
 
 			case 2:
-			out.red = base;
-			out.green = val;
-			out.blue = (((val-base)*(hue%256))/256)+base;
+			color.red = base;
+			color.green = val;
+			color.blue = (((val-base)*(hue%256))/256)+base;
 			break;
 
 			case 3:
-			out.red = base;
-			out.green = (((val-base)*(256-(hue%256)))/256)+base;
-			out.blue = val;
+			color.red = base;
+			color.green = (((val-base)*(256-(hue%256)))/256)+base;
+			color.blue = val;
 			break;
 
 			case 4:
-			out.red = (((val-base)*(hue%256))/256)+base;
-			out.green = base;
-			out.blue = val;
+			color.red = (((val-base)*(hue%256))/256)+base;
+			color.green = base;
+			color.blue = val;
 			break;
 
 			case 5:
-			out.red = val;
-			out.green = base;
-			out.blue = (((val-base)*(256-(hue%256)))/256)+base;
+			color.red = val;
+			color.green = base;
+			color.blue = (((val-base)*(256-(hue%256)))/256)+base;
 			break;
 		}
 	}
-	
-	return out;
 } // HSV 2 RGB Binary
 
 ISR(INT0_vect)
